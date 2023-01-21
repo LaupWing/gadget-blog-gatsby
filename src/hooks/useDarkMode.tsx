c13046bf03b087a48f7311c79902944cd4e9dea1
LaupWing <loc_strict@hotmail.com>
2023-01-21 17:46:10 +0100

    udpate

---
+++ src/hooks/useDarkMode.tsx
@@ -0,0 +1,22 @@
+import React, { createContext, useState, Dispatch, SetStateAction } from "react"
+
+interface State {
+   darkMode: boolean
+   setDarkMode: Dispatch<SetStateAction<boolean>>
+}
+
+const DarkModeContext = createContext<State>({
+   darkMode:true,
+   setDarkMode: () => {}
+}) 
+
+export const DarkModeProvider:React.FC<React.PropsWithChildren> = ({ children }) => {
+   const [darkMode, setDarkMode] = useState(true)
+   const value = {darkMode, setDarkMode}
+
+   return (
+      <DarkModeContext.Provider value={value}>
+         {children}
+      </DarkModeContext.Provider>
+   )
+}
