fabde51a0a8f558551b3cc943dd283bdbe429849
LaupWing <loc_strict@hotmail.com>
2023-01-22 19:10:29 +0100

    update

--- src/components/Slideshow/Slideshow.tsx
+++ src/components/Slideshow/Slideshow.tsx
@@ -4,15 +4,18 @@ import Slide1 from "./Slide1"
 
 const Slideshow = () => {
    return (
-      <Carousel 
-         showThumbs={false}
-         showStatus={false}
-         
-      >
-         <Slide1/>
-         <Slide1/>
-         <Slide1/>
-      </Carousel>
+      <div className="relative">
+         <img className="absolute inset-0 opacity-5 w-full h-full object-cover" src="https://i.imgur.com/wblnWBh.png" alt="carousel background" />
+         <Carousel 
+            showThumbs={false}
+            showStatus={false}
+            
+         >
+            <Slide1/>
+            <Slide1/>
+            <Slide1/>
+         </Carousel>
+      </div>
    )
 }
 export default Slideshow
--- src/components/Slideshow/_SlideContainer.tsx
+++ src/components/Slideshow/_SlideContainer.tsx
@@ -6,7 +6,6 @@ const SlideContainer:React.FC<React.PropsWithChildren> = ({ children }) => {
          <div className="container flex my-4">
             {children}
          </div>
-         <img className="absolute inset-0 opacity-5 -z-10 h-full object-cover" src="https://i.imgur.com/wblnWBh.png" alt="carousel background" />
       </div>
    )
 }
