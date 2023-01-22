e9d0039e507313e74b287917a7745a02f42a1e3e
LaupWing <loc_strict@hotmail.com>
2023-01-22 18:57:23 +0100

    added

--- src/components/Layout/Slideshow/Slideshow.tsx
+++ src/components/Layout/Slideshow/Slideshow.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { Carousel } from "react-responsive-carousel"
+import Slide1 from "./Slide1"
 
 const Slideshow = () => {
    return (
@@ -8,7 +9,9 @@ const Slideshow = () => {
          showStatus={false}
          
       >
-
+         <Slide1/>
+         <Slide1/>
+         <Slide1/>
       </Carousel>
    )
 }
