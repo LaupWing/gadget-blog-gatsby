50e28240b92eec09a0b50bf243c1bfe916148be6
LaupWing <loc_strict@hotmail.com>
2023-01-22 17:59:29 +0100

    imports

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { DarkModeProvider } from "../hooks/useDarkMode"
+import "react-responsive-carousel/lib/styles/carousel.min.css"
 
 const Layout:React.FC<React.PropsWithChildren> = ({ children }) => {
    return (
