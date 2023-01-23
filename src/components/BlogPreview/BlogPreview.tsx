0533585bcd7e578c4657373394a7ffe5f918c88d
LaupWing <loc_strict@hotmail.com>
2023-01-23 19:02:04 +0100

    udpate

--- src/components/BlogPreview/BlogPreview.tsx
+++ src/components/BlogPreview/BlogPreview.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
 import { BlogType } from "../../../types"
 
 const PostPreview:React.FC<{data: BlogType}> = ({data}) => {
-   console.log(getImage(data.featuredImage.node))
+   console.log(data)
    return (
       <div className="flex flex-col rounded p-1.5 relative border-transparent border-2 hover:border-neutral-700 group duration-150 cursor-pointer col-span-1">
          <GatsbyImage
