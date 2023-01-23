6f4d2cbb1d114ce43d2481cbfcf43ee2fa415aaa
LaupWing <loc_strict@hotmail.com>
2023-01-23 17:27:10 +0100

    update

--- src/hooks/useLatestBlogQuery.tsx
+++ src/hooks/useLatestBlogQuery.tsx
@@ -3,13 +3,30 @@ import { graphql, useStaticQuery } from "gatsby"
 export const useLatestBlogQuery = () =>{
    const data = useStaticQuery(graphql`
       query {
-         allWpPost(sort: { fields: date, order: DESC }){
-            edges {
-               node {
-                  title
+         allWpPost(
+            sort: { fields: date, order: DESC }
+            limit: 4
+         ){
+            nodes {
+               uri
+               title
+               excerpt
+               id
+               slug
+               dateGmt
+               date(fromNow: true)
+               featuredImage {
+                  node {
+                     localFile {
+                        childImageSharp {
+                        gatsbyImageData(width: 720, placeholder: TRACED_SVG)
+                        }
+                     }
+                  }
                }
             }
          }
       }
    `)
+   return data
 }
