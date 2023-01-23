ee1112ac2c41a77d59df664d34a5ba372ab0cc60
LaupWing <loc_strict@hotmail.com>
2023-01-23 18:39:12 +0100

    update

--- src/components/BlogPreview/BlogPreview.tsx
+++ src/components/BlogPreview/BlogPreview.tsx
@@ -3,6 +3,7 @@ import * as React from "react"
 
 const PostPreview = (data: any) => {
    console.log(data)
+   
    return (
       <div className="flex flex-col rounded p-1.5 relative border-transparent border-2 hover:border-neutral-700 group duration-150 cursor-pointer col-span-1">
          {/* <GatsbyImage
