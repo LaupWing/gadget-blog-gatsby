d29dcd16ee707551e20fb58a7f7cd4f17d5b1a28
LaupWing <loc_strict@hotmail.com>
2023-01-23 18:17:49 +0100

    updateing

--- src/components/LatestBlogs/LatestBlogs.tsx
+++ src/components/LatestBlogs/LatestBlogs.tsx
@@ -1,17 +1,18 @@
 import { useLatestBlogQuery } from "../../hooks/useLatestBlogQuery"
 import * as React from "react"
-import PostPreview from "../BlogPreview/BlogPreview"
+import BlogPreview from "../BlogPreview/BlogPreview"
 
 const LatestBlogs = () => {
    const data = useLatestBlogQuery()
-
+   
    return (
       <div className="container py-10">
          <h2 className="text-white text-2xl font-bold">Latest blogs</h2>
          <div className="grid sm:grid-cols-2 grid-cols-1 my-5">
-            {data.map(x => (
-               <PostPreview
+            {data.allWpPost.nodes.map((x:any) => (
+               <BlogPreview
                   data={x}
+                  key={x.id}
                />
             ))}
          </div>
