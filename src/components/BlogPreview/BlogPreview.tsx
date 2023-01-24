db49af582dc0927db66054a5f5046c77caea8e8e
LaupWing <loc_strict@hotmail.com>
2023-01-24 17:06:56 +0100

    time

--- src/components/BlogPreview/BlogPreview.tsx
+++ src/components/BlogPreview/BlogPreview.tsx
@@ -11,7 +11,10 @@ const BlogPreview:React.FC<{data: BlogType}> = ({data}) => {
             alt="Preview image"
             className="aspect-[6/4]"
          />
-         <time className="group-hover:text-indigo-600 text-neutral-200 absolute top-3 right-3 uppercase text-xs font-bold z-20" dateTime={data.dateGmt}>
+         <time 
+            className="group-hover:text-indigo-600 text-neutral-600 absolute top-3 right-3 uppercase text-xs font-bold z-20 bg-white/80 backdrop-blur-lg px-2 py-[1px] rounded" 
+            dateTime={data.dateGmt}
+         >
             {data.date}
          </time>
          <h1 className="py-2 group-hover:text-indigo-500 text-neutral-400 font-bold text-md">
