78b075da4de83b7cf54ddaa997475c0c24a2fcb8
LaupWing <loc_strict@hotmail.com>
2023-01-24 16:54:21 +0100

    update

--- src/components/BlogPreview/BlogPreview.tsx
+++ src/components/BlogPreview/BlogPreview.tsx
@@ -2,7 +2,7 @@ import { getImage, GatsbyImage, } from "gatsby-plugin-image"
 import * as React from "react"
 import { BlogType } from "../../../types"
 
-const PostPreview:React.FC<{data: BlogType}> = ({data}) => {
+const BlogPreview:React.FC<{data: BlogType}> = ({data}) => {
    console.log(data)
    return (
       <div className="flex flex-col rounded p-1.5 relative border-transparent border-2 hover:border-neutral-700 group duration-150 cursor-pointer col-span-1">
@@ -17,8 +17,8 @@ const PostPreview:React.FC<{data: BlogType}> = ({data}) => {
          <h1 className="py-2 group-hover:text-indigo-500 text-neutral-400 font-bold text-md">
             {data.title}
          </h1>
-         
+
       </div>
    )
 }
-export default PostPreview
+export default BlogPreview
