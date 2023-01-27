daf8f44299b8c1d1a5309294a8e3c7e706b21da1
LaupWing <loc_strict@hotmail.com>
2023-01-27 18:52:39 +0100

    update

---
+++ src/components/CommentSection/index.tsx
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+const CommentSection = () => {
+   return (
+      <div className="w-full flex flex-col">
+         <h4 className="uppercase font-semibold text-sm text-neutral-500 mb-4">Comments</h4>
+         <div className="flex flex-col">
+
+         </div>
+      </div>
+   )
+}
+export default CommentSection
