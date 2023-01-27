0d178a6f92684ac325cbc42699952905284ba8b9
LaupWing <loc_strict@hotmail.com>
2023-01-27 18:57:52 +0100

    update

---
+++ src/components/CommentSection/Comment.tsx
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+const Comment = ({comment, setReply, reply}:any) => {
+   return (
+      <div className={`flex flex-col py-3 mb-4 px-2 rounded border shadow ${
+         reply === comment 
+            ? "border-indigo-600" 
+            : "border-neutral-200"}`
+         }
+      >
+         Comment
+      </div>
+   )
+}
+export default Comment
