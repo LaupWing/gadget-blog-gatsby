f2e453f5ca04cbac69a8292a2aa0ada92285a58d
LaupWing <loc_strict@hotmail.com>
2023-01-27 16:33:32 +0100

    create pages

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -10,15 +10,61 @@
 import { CreatePagesArgs } from "gatsby"
 import path from "path"
 
-export const createPages = async ({ actions }: CreatePagesArgs) => {
+export const createPages = async ({ actions, graphql, reporter }: CreatePagesArgs) => {
    const { createPage } = actions
 
    const blogPostTemplate = path.resolve("./src/templates/blog-post.tsx")
+
+   const result = await graphql(`
+      query {
+         wp {
+            readingSettings {
+               postsPerPage
+            }
+         }
+         allWpCategory {
+            edges {
+               node {
+                  name
+                  id
+                  uri
+                  slug
+                  count
+               }
+            }
+         }
+      }
+   `)
+
+   if(result.errors){
+      reporter.panicOnBuild("Something went wrong during the creation", result.errors)
+      return
+   }
+
+   const {wp, allWpCategory} = result.data as any
    
-   createPage({
-      path: "/using-dsg",
-      component: path.resolve("./src/templates/using-dsg.tsx"),
-      context: {},
-      defer: true,
+   allWpCategory.edges.forEach((category:any) => {
+      const postsPerPage = wp.readingSettings.postsPerPage
+      const numberOfPosts = category.node.count
+      const numberOfPages = Math.ceil(numberOfPosts / postsPerPage)
+
+      if(numberOfPosts > 0 || category.node.name !== "uncategorized"){
+         Array.from({length: numberOfPages}).forEach((_:any, i:number)=>{
+            createPage({
+               path: i === 0
+                  ? category.node.uri
+                  : `${category.node.uri}${i + 1}`,
+               component: blogPostTemplate,
+               context: {
+                  limit: postsPerPage,
+                  skip: i * postsPerPage,
+                  numberOfPages,
+                  currentPage: i + 1,
+                  category,
+                  allCategories: allWpCategory
+               }
+            })
+         })
+      }
    })
 }
