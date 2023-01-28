0c46938fa9cbbd641828c2083cb5c906e67e1841
LaupWing <loc_strict@hotmail.com>
2023-01-28 12:10:32 +0100

    opiacty is foke dup

--- src/components/Slideshow/Slideshow.tsx
+++ src/components/Slideshow/Slideshow.tsx
@@ -1,3 +1,4 @@
+import { StaticImage } from "gatsby-plugin-image"
 import * as React from "react"
 import { Carousel } from "react-responsive-carousel"
 import Slide1 from "./Slide1"
@@ -5,7 +6,15 @@ import Slide1 from "./Slide1"
 const Slideshow = () => {
    return (
       <div className="relative">
-         <img className="absolute inset-0 opacity-5 w-full h-full object-cover" src="https://i.imgur.com/wblnWBh.png" alt="carousel background" />
+         {/* <img className="absolute inset-0 opacity-5 w-full h-full object-cover" src="https://i.imgur.com/wblnWBh.png" alt="carousel background" /> */}
+         <StaticImage
+            src="../../images/pattern.png"
+            placeholder="blurred"
+            quality={95}
+            loading="eager"
+            className="absolute inset-0 z-50 opacity-5 w-full h-full object-cover"
+            alt="Blog Hero"
+         />
          <Carousel 
             showThumbs={false}
             showStatus={false}
