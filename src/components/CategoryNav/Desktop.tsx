a072671eb80c5d8426ba1431c54b2fb571331019
LaupWing <loc_strict@hotmail.com>
2023-01-28 13:33:38 +0100

    siidenav

--- src/components/CategoryNav/Desktop.tsx
+++ src/components/CategoryNav/Desktop.tsx
@@ -3,20 +3,20 @@ import * as React from "react"
 import { Category } from "../../../types"
 import Icons from "../Icons"
 
-const Desktop:React.FC<{categories: Category[]}> = ({categories}) => {
+const Desktop: React.FC<{ categories: Category[] }> = ({ categories }) => {
    return (
-      <nav className='sm:flex flex-col pl-4 hidden'>
-         <h2 className='font-bold uppercase mb-4 text-neutral-600'>Blog</h2>
-         <ul className='space-y-2 flex flex-col text-neutral-400 '>
-            {categories.map(x=>(
+      <nav className="sm:flex flex-col pl-4 hidden">
+         <h2 className="font-bold uppercase mb-4 text-neutral-900">Blog</h2>
+         <ul className="space-y-2 flex flex-col text-neutral-400 ">
+            {categories.map(x => (
                <Link
                   key={x.node.id}
                   to={x.node.uri}
-                  className='flex items-center justify-between hover:text-indigo-600 group'
-                  activeClassName='text-indigo-600 active'
+                  className="flex items-center justify-between hover:text-violet-500 group"
+                  activeClassName="text-violet-600 active"
                >
-                  <li className='text-sm uppercase font-bold'>{x.node.name}</li>
-                  <Icons className="ml-10" name={x.node.name}/>
+                  <li className="text-sm uppercase font-bold">{x.node.name}</li>
+                  <Icons className="ml-10" name={x.node.name} />
                </Link>
             ))}
          </ul>
--- src/components/LatestBlogs/LatestBlogs.tsx
+++ src/components/LatestBlogs/LatestBlogs.tsx
@@ -1,14 +1,13 @@
 import { useLatestBlogQuery } from "../../hooks/useLatestBlogQuery"
 import * as React from "react"
 import BlogPreview from "../BlogPreview/BlogPreview"
-import { BlogType } from "../../../types"
 
 const LatestBlogs = () => {
    const data = useLatestBlogQuery()
    
    return (
       <div className="container py-10">
-         <h2 className="text-white text-2xl font-bold">Latest blogs</h2>
+         <h2 className="text-neutral-900 text-2xl font-bold">Latest blogs</h2>
          <div className="grid sm:grid-cols-2 grid-cols-1 my-5 gap-y-10 gap-x-4">
             {data.allWpPost.nodes.map((x:any) => (
                <BlogPreview
