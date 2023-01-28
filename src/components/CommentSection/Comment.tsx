86ebbb913481b2ddb3824cea658ac3fe37b1a5fb
LaupWing <loc_strict@hotmail.com>
2023-01-28 17:13:13 +0100

    just copied that shiiiit

--- src/components/CommentSection/Comment.tsx
+++ src/components/CommentSection/Comment.tsx
@@ -1,24 +1,47 @@
 import Avatar from "boring-avatars"
 import * as React from "react"
 
-const Comment = ({comment, setReply, reply}:any) => {
+const Comment = ({ comment, setReply, reply }: any) => {
    return (
-      <div className={`flex flex-col py-3 mb-4 px-2 rounded border shadow ${
-         reply === comment 
-            ? "border-indigo-600" 
-            : "border-neutral-200"}`
-         }
+      <div
+         className={`flex flex-col py-3 mb-4 px-2 rounded border shadow ${
+            reply === comment ? "border-indigo-600" : "border-neutral-200"
+         }`}
       >
-         <CommentContent
-            comment={comment}
-            avatarSize={60}
-         />
+         <CommentContent comment={comment} avatarSize={60} />
+         <div className="flex flex-col">
+            {comment.replies.nodes.map((c: any) => (
+               <div className="flex items-center" key={c.id}>
+                  <div className="h-0.5 rounded-full mx-2 w-10 bg-neutral-300"></div>
+                  <CommentContent
+                     comment={c}
+                     avatar_size={50}
+                     className="my-4"
+                  />
+               </div>
+            ))}
+         </div>
+         {reply === comment ? (
+            <button
+               className="ml-auto uppercase text-sm font-bold tracking-wider text-indigo-300 hover:text-indigo-600"
+               onClick={() => setReply(false)}
+            >
+               Cancel Reply
+            </button>
+         ) : (
+            <button
+               className="ml-auto uppercase text-sm font-bold tracking-wider text-indigo-300 hover:text-indigo-600"
+               onClick={() => setReply(comment)}
+            >
+               Reply
+            </button>
+         )}
       </div>
    )
 }
 export default Comment
 
-const CommentContent = ({comment, avatarSize, className=""}:any) => {
+const CommentContent = ({ comment, avatarSize, className = "" }: any) => {
    return (
       <div className={"flex-1 flex " + className}>
          <Avatar
@@ -26,6 +49,17 @@ const CommentContent = ({comment, avatarSize, className=""}:any) => {
             name={comment.author.node.name}
             variant={"beam"}
          />
+         <div className="ml-2 flex flex-col flex-1">
+            <div className="flex justify-between flex-1 items-center">
+               <h3 className="font-bold uppercase text-sm">
+                  {comment.author.node.name}
+               </h3>
+               <p className="uppercase font-bold text-xs text-neutral-400">
+                  {comment.date}
+               </p>
+            </div>
+            <p dangerouslySetInnerHTML={{ __html: comment.content }}></p>
+         </div>
       </div>
    )
 }
--- src/components/CommentSection/index.tsx
+++ src/components/CommentSection/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import AddComment from "./AddComment"
+import Comment from "./Comment"
 
 const CommentSection = ({post}:any) => {
    const [reply, setReply] = useState(false)
@@ -7,7 +8,14 @@ const CommentSection = ({post}:any) => {
       <div className="w-full flex flex-col">
          <h4 className="uppercase font-semibold text-sm text-neutral-500 mb-4">Comments</h4>
          <div className="flex flex-col">
-
+            {post.comments.nodes.filter((x:any)=>!x.parentId).map((comment:any)=>
+               <Comment 
+                  key={comment.id} 
+                  comment={comment}
+                  setReply={setReply}
+                  reply={reply}
+               />
+            )}
          </div>
          <AddComment 
             post={post} 
