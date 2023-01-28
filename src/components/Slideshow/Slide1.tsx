c08e22acd4ff43801a114594fdf2ae32c632ab58
LaupWing <loc_strict@hotmail.com>
2023-01-28 13:42:14 +0100

    udpate

--- src/components/Slideshow/Slide1.tsx
+++ src/components/Slideshow/Slide1.tsx
@@ -4,7 +4,10 @@ import SlideContainer from "./_SlideContainer"
 const Slide1 = () => {
    return (
       <SlideContainer>
-         <div></div>
+         <div className="bg-green-300 m-10 border-2 text-left max-w-md border-black p-6 rounded-xl shadow-hard-lg">
+            <h1 className="text-2xl mb-2">Welcome to <strong>TechHeaven</strong></h1>
+            <p className="leading-snug">Our objective is to help readers make informed purchasing decisions by providing in-depth reviews, real-world examples, and comprehensive comparisons.</p>
+         </div>
       </SlideContainer>
    )
 }
--- src/components/Slideshow/_SlideContainer.tsx
+++ src/components/Slideshow/_SlideContainer.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
 const SlideContainer:React.FC<React.PropsWithChildren> = ({ children }) => {
    return (
       <div className="flex relative h-[60vh]">
-         <div className="container flex my-4">
+         <div className="container flex my-4 items-start">
             {children}
          </div>
       </div>
