a4f2da47e07b92e6ab03a352c09a2b7856792fa9
LaupWing <loc_strict@hotmail.com>
2023-01-28 13:51:22 +0100

    added logo

--- src/components/Slideshow/Slideshow.tsx
+++ src/components/Slideshow/Slideshow.tsx
@@ -6,13 +6,12 @@ import Slide1 from "./Slide1"
 const Slideshow = () => {
    return (
       <div className="relative">
-         {/* <img className="absolute inset-0 opacity-5 w-full h-full object-cover" src="https://i.imgur.com/wblnWBh.png" alt="carousel background" /> */}
          <StaticImage
             src="../../images/pattern.png"
             placeholder="blurred"
             quality={95}
             loading="eager"
-            className="absolute inset-0 w-full h-full object-cover"
+            className="absolute opacity-20 inset-0 w-full h-full object-cover"
             alt="Blog Hero"
          />
          <Carousel 
