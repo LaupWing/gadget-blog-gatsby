806422f3752eff7e0f611a3b97f0bbf930a304c6
LaupWing <loc_strict@hotmail.com>
2023-01-28 18:56:49 +0100

    udpate

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,11 +1,13 @@
 import * as React from "react"
-// import { DarkModeProvider } from "../hooks/useDarkMode"
 import "react-responsive-carousel/lib/styles/carousel.min.css"
+import 'react-notifications-component/dist/theme.css'
 import Layout from "../components/Layout/Layout"
+import { ReactNotifications } from "react-notifications-component"
 
 const LayoutTemplate:React.FC<React.PropsWithChildren> = ({ children }) => {
    return (
       <Layout>
+         <ReactNotifications />
          {children}
       </Layout>
    )
